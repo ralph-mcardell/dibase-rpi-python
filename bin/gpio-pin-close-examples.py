67c84ee0a87cff423b6f0a0da37a0bbdd1e90cb9
Ralph McArdell <ralph@dibasepi1>
2012-08-20 17:34:34 +0100

    Corrected except error.GPIOError clause to include e name for excepotionobject (reference). Added pin group close examples.

--- bin/gpio-pin-close-examples.py
+++ bin/gpio-pin-close-examples.py
@@ -2,11 +2,11 @@
 '''
     Example uses of the dibase.rpi.gpio package modules.
 
-    Shows examples of closing open pin IO objects returned from the
-    pin.open_pin function.
+    Shows examples of closing open pin and pin group IO objects returned
+    from the pin.open_pin and pingroup.open_pingroup functions.
     
     Also demonstrates the use of pin.force_free_pin to forably ensure a pin
-    IO object is closed and determining if it had to be forably closed.
+    IO object is closed and determining if it had to be forcably closed.
 
     Developed by R.E. McArdell / Dibase Limited.
     Copyright (c) 2012 Dibase Limited
@@ -17,6 +17,7 @@ import sys
 if __name__ == '__main__':
     sys.path.insert(0, './..')
 import dibase.rpi.gpio.pin as pin
+import dibase.rpi.gpio.pingroup as pingroup
 import dibase.rpi.gpio.gpioerror as error
 
 def open_pin_explicit_close():
@@ -25,9 +26,22 @@ def open_pin_explicit_close():
     gpio_gen0_out.close()
     assert( gpio_gen0_out.closed() )
 
+def open_pingroup_explicit_close():
+    gpio_gen0_1_out = pingroup.open_pingroup( [ pin.PinId.p1_gpio_gen0()
+                                              , pin.PinId.p1_gpio_gen1()
+                                              ]
+                                            , 'w')
+    assert( not gpio_gen0_1_out.closed() )
+    gpio_gen0_1_out.close()
+    assert( gpio_gen0_1_out.closed() )
+
 def open_pin_implicit_close( pinId ):
     pin_in = pin.open_pin(pinId)
     assert( not pin_in.closed() )
+
+def open_pingroup_implicit_close( pinIds ):
+    pins_in = pingroup.open_pingroup(pinIds)
+    assert( not pins_in.closed() )
 
 def with_open_pin_closed_in_with_clause_exit_cleanup():
     outside_value = None
@@ -37,15 +51,42 @@ def with_open_pin_closed_in_with_clause_exit_cleanup():
         assert( not outside_value.closed() )
     assert( outside_value.closed() )
 
+def with_open_pingroup_closed_in_with_clause_exit_cleanup():
+    outside_value = None
+    with pingroup.open_pingroup( [ pin.PinId.p1_gpio_gen0()
+                                 , pin.PinId.p1_gpio_gen1()
+                                 ]
+                               ) as p:
+        outside_value = p
+        assert( not p.closed() )
+        assert( not outside_value.closed() )
+    assert( outside_value.closed() )
+
 if __name__ == '__main__':
     try:
         open_pin_explicit_close()    
+        open_pingroup_explicit_close()    
+
         with_open_pin_closed_in_with_clause_exit_cleanup()
+        with_open_pingroup_closed_in_with_clause_exit_cleanup()
+
         open_pin_implicit_close(pin.PinId.p1_gpio_gen1())
     # Check requested pin opened by open_pin_implicit_close is closed
     # by checking return value from pin.force_free_pin:
         freed_pin_id = pin.force_free_pin(pin.PinId.p1_gpio_gen1())
         assert( freed_pin_id==None )
+
+        open_pingroup_implicit_close( [ pin.PinId.p1_gpio_gen0()
+                                      , pin.PinId.p1_gpio_gen1()
+                                      ]
+                                    )
+    # Check requested pins opened by open_pingroup_implicit_close are closed
+    # by checking return value from pin.force_free_pin:
+        freed_pin_id = pin.force_free_pin(pin.PinId.p1_gpio_gen0())
+        assert( freed_pin_id==None )
+        freed_pin_id = pin.force_free_pin(pin.PinId.p1_gpio_gen1())
+        assert( freed_pin_id==None )
+
     except error.GPIOError,e:
         print "Oops unexpected GPIO related error:\n   ",\
                 e.__class__.__name__,':', e
