9a3afb172b777d4e074d70db098b3f71b24569e7
Ralph McArdell <ralph@dibasepi1>
2013-05-23 15:51:07 +0100

    Fixed to work with revision index parameter to RPiPinIdSet.valid_ids, replaced System with Platform, split out interactive tests to their own modules.

--- dibase/rpi/gpio/test/pin-platformtests.py
+++ dibase/rpi/gpio/test/pin-platformtests.py
@@ -1,16 +1,11 @@
 '''
     Part of the dibase.rpi.gpio.test package.
 
-    System tests on read/write operations on single GPIO pin
+    Platform tests on read/write operations on single GPIO pin
     IO type instances.
-    
-    ---------------------------------------------------------------
-    | NOTE: Some tests require GPIO input line state modification |
-    |       that probably implies user interaction.               |
-    ---------------------------------------------------------------
 
     Developed by R.E. McArdell / Dibase Limited.
-    Copyright (c) 2012 Dibase Limited
+    Copyright (c) 2012,2013 Dibase Limited
     License: dual: GPL or BSD.
 '''
 
@@ -24,10 +19,10 @@ from dibase.rpi.gpio import pin
 from dibase.rpi.gpio import pinid
 from dibase.rpi.gpio import gpioerror as error
 
-class PinWriterSystemTests(unittest.TestCase):
+class PinWriterPlatformTests(unittest.TestCase):
     def tearDown(self):
         cleaned_up = []
-        for v in pinid.RPiPinIdSet.valid_ids():
+        for v in pinid.RPiPinIdSet.valid_ids(pin.PinId._get_rpi_major_revision_index()):
             id = pin.force_free_pin(pin.PinId.gpio(v))
             if (id!=None):
                 cleaned_up.append(id)
@@ -117,10 +112,10 @@ class PinWriterSystemTests(unittest.TestCase):
             self.assertFalse( outside_value.closed() )
         self.assertTrue( outside_value.closed() )
 
-class PinReaderSystemTests(unittest.TestCase):
+class PinReaderPlatformTests(unittest.TestCase):
     def tearDown(self):
         cleaned_up = []
-        for v in pinid.RPiPinIdSet.valid_ids():
+        for v in pinid.RPiPinIdSet.valid_ids(pin.PinId._get_rpi_major_revision_index()):
             id = pin.force_free_pin(pin.PinId.gpio(v))
             if (id!=None):
                 cleaned_up.append(id)
@@ -211,10 +206,10 @@ class PinReaderSystemTests(unittest.TestCase):
         self.assertTrue( outside_value.closed() )
 
 
-class PinBlockingReaderSystemTests(unittest.TestCase):
+class PinBlockingReaderPlatformTests(unittest.TestCase):
     def tearDown(self):
         cleaned_up = []
-        for v in pinid.RPiPinIdSet.valid_ids():
+        for v in pinid.RPiPinIdSet.valid_ids(pin.PinId._get_rpi_major_revision_index()):
             id = pin.force_free_pin(pin.PinId.gpio(v))
             if (id!=None):
                 cleaned_up.append(id)
@@ -304,10 +299,10 @@ class PinBlockingReaderSystemTests(unittest.TestCase):
             self.assertFalse( outside_value.closed() )
         self.assertTrue( outside_value.closed() )
 
-class OpenFunctionSystemTests(unittest.TestCase):
+class OpenFunctionPlatformTests(unittest.TestCase):
     def tearDown(self):
         cleaned_up = []
-        for v in pinid.RPiPinIdSet.valid_ids():
+        for v in pinid.RPiPinIdSet.valid_ids(pin.PinId._get_rpi_major_revision_index()):
             id = pin.force_free_pin(pin.PinId.gpio(v))
             if (id!=None):
                 cleaned_up.append(id)
@@ -363,136 +358,6 @@ class OpenFunctionSystemTests(unittest.TestCase):
     def test_open_pin_bad_mode_string_fails(self):
         with self.assertRaises( error.PinOpenModeInvalidError ):
             self.assertIsInstance(pin.open_pin(pin.PinId.p1_gpio_gen0(),'rN+'),pin.PinWriter)
-
-class XPinIOInteractiveSystemTests(unittest.TestCase):
-    def tearDown(self):
-        cleaned_up = []
-        for v in pinid.RPiPinIdSet.valid_ids():
-            id = pin.force_free_pin(pin.PinId.gpio(v))
-            if (id!=None):
-                cleaned_up.append(id)
-        if ( cleaned_up != [] ):
-            print "\nCleaned up left over exports for pins", cleaned_up
-        self.assertEqual(cleaned_up,[])
-
-    def test_050_write_to_open_pin_allows_various_values_in_on_off_on_off_etc_sequence(self):
-        a_pin = pin.PinWriter( pin.PinId.p1_gpio_gen0() )
-        self.assertFalse( a_pin.closed() )
-        # if hardware pin GPIO_GEN0 on the Raspberry Pi P1 connector is
-        # connected to an indicator such as as LED that lights for a 1 output
-        # then it should flash ON OFF ON OFF ON OFF ON OFF in approximately a
-        # .2 second cycle time.
-        print "\nToggling GPIO_GEN0 (Raspberry Pi P1 pin 11): ON OFF ON OFF ON OFF ON OFF ..."
-        time.sleep(1.5)
-        a_pin.write(1)
-        time.sleep(0.2)
-        a_pin.write(0)
-        time.sleep(0.2)
-        a_pin.write(True)
-        time.sleep(0.2)
-        a_pin.write(False)
-        time.sleep(0.2)
-        a_pin.write(23)
-        time.sleep(0.2)
-        a_pin.write(None)
-        time.sleep(0.2)
-        a_pin.write('1')
-        time.sleep(0.2)
-        a_pin.write('0')
-        time.sleep(0.2)
-        a_pin.close()
-        print "Test complete.\n"
-
-    def test_100_read_from_open_pin_non_blocking_returns_True_or_False(self):
-        a_pin = pin.PinReader( pin.PinId.p1_gpio_gclk() )
-        self.assertFalse( a_pin.closed() )
-        # Read a value from the input pin and check it is False or True.
-        # Output this value to the console so if hardware pin GPIO_GCLK
-        # on the Raspberry Pi P1 connector is connected to a switch then
-        # you can check the read value concurs with the switch state.
-        value = a_pin.read()
-        self.assertIn(value, [False, True])
-        print '\nPinReader object on GPIO_GCLK: read value:', value
-        print "Test complete.\n"
-
-    def test_150_read_from_open_pin_blocking_returns_True_or_False(self):
-        a_pin = pin.PinBlockingReader( pin.PinId.p1_gpio_gclk(), 'B' )
-        self.assertFalse( a_pin.closed() )
-        # Read a value from the input pin and check it is False or True.
-        # Output this value to the console so if hardware pin GPIO_GCLK
-        # on the Raspberry Pi P1 connector is connected to a switch then
-        # you can check the read value concurs with the switch state.
-        value = a_pin.read()
-        self.assertIn(value, [False, True])
-        print '\nPinBlockingReader object on GPIO_GCLK: read value:', value
-        print "Test complete.\n"
-
-    def test_200_read_from_open_pin_non_blocking_more_than_once_returns_expected_results(self):
-        a_pin = pin.PinReader( pin.PinId.p1_gpio_gclk() )
-        self.assertFalse( a_pin.closed() )
-        print "\nSet GPIO_GCLK (Raspberry Pi P1 pin7) HIGH..."
-        time.sleep(3.0)
-        value = a_pin.read()
-        self.assertTrue(value)
-        print "Set GPIO_GCLK (Raspberry Pi P1 pin7) LOW..."
-        time.sleep(3.0)
-        value = a_pin.read()
-        self.assertFalse(value)
-        print "Test complete.\n"
-
-    def test_250_blocking_reader_zero_time_out_returns_polled_pin_state(self):
-        a_pin = pin.PinBlockingReader( pin.PinId.p1_gpio_gclk(), 'B' )
-        self.assertFalse( a_pin.closed() )
-        a_pin.read() # clear initially signalled notification
-        self.assertIn( a_pin.read(0), [False, True] )
-        self.assertFalse( a_pin.closed() )
-
-    def test_260_blocking_reader_time_out_expiration_returns_None(self):
-        a_pin = pin.PinBlockingReader( pin.PinId.p1_gpio_gclk(), 'B' )
-        self.assertFalse( a_pin.closed() )
-        a_pin.read() # clear initially signalled notification
-        self.assertIsNone( a_pin.read(0.001) )
-        self.assertFalse( a_pin.closed() )
-
-    def test_300_blocking_reader_wait_both_no_time_out_notifies_and_reads_results_repeatedly_on_any_state_change(self):
-        a_pin = pin.PinBlockingReader( pin.PinId.p1_gpio_gclk(), 'B' )
-        self.assertFalse( a_pin.closed() )
-        a_pin.read() # Clear intial signalled event.
-        print "\nChange value of GPIO_GCLK (Raspberry Pi P1 pin7) twice (e,g. press and release)..."
-        value = a_pin.read()
-        self.assertIn(value, [False, True])
-        print 'PinBlockingReader object on GPIO_GCLK: read value:', value
-        value = a_pin.read(1000)
-        self.assertIn(value, [False, True])
-        print 'PinBlockingReader object on GPIO_GCLK: read value:', value
-        print "Test complete.\n"
-
-    def test_350_blocking_reader_wait_rising_edge_no_time_out_notifies_and_reads_results_repeatedly_on_0_1_state_change(self):
-        a_pin = pin.PinBlockingReader( pin.PinId.p1_gpio_gclk(), 'R' )
-        self.assertFalse( a_pin.closed() )
-        a_pin.read() # Clear intial signalled event.
-        print "\nChange value of GPIO_GCLK (Raspberry Pi P1 pin 7) from 0 to 1 twice (e.g. press, release, press, release)..."
-        value = a_pin.read()
-        self.assertEqual(value, True)
-        print 'PinBlockingReader object on GPIO_GCLK: read value:', value
-        value = a_pin.read()
-        self.assertEqual(value, True)
-        print 'PinBlockingReader object on GPIO_GCLK: read value:', value
-        print "Test complete.\n"
-
-    def test_400_blocking_reader_wait_falling_edge_no_time_out_notifies_and_reads_results_repeatedly_on_1_0_state_change(self):
-        time.sleep(0.3)
-        a_pin = pin.PinBlockingReader( pin.PinId.p1_gpio_gclk(), 'F' )
-        self.assertFalse( a_pin.closed() )
-        a_pin.read() # Clear intial signalled event.
-        print "\nChange value of GPIO_GCLK (Raspberry Pi P1 pin 7) from 1 to 0 twice (e.g. press, release, press, release)..."
-        value = a_pin.read()
-        self.assertEqual(value, False)
-        print 'PinBlockingReader object on GPIO_GCLK: read value:', value
-        value = a_pin.read()
-        self.assertEqual(value, False)
-        print 'PinBlockingReader object on GPIO_GCLK: read value:', value
-        print "Test complete.\n"
 
 if __name__ == '__main__':
     unittest.main()
